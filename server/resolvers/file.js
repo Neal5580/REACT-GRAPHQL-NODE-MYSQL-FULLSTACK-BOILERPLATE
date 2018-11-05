82da96231c9aa0bab6994da205ceec8857825456
Neal Shen <>
2018-11-05 11:57:25 +1100

    file upload to s3 buckets

--- server/resolvers/file.js
+++ server/resolvers/file.js
@@ -1,15 +1,55 @@
-const { createWriteStream } = require("fs");
+import AWS from "aws-sdk";
+import uuid from "uuid";
+import { AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID } from "../keys";
 
 export default {
     Mutation: {
         async uploadFile(parent, { file }) {
             console.log("Mutation:uploadFile");
-
             const { stream, filename, mimetype, encoding } = await file;
 
-            console.log(filename);
+            //Configuring the SDK & S3
+            const albumBucketName = "digitalconcierge";
+            const region = "ap-southeast-2";
 
-            console.log(stream);
+            AWS.config.update({
+                secretAccessKey: AWS_SECRET_ACCESS_KEY,
+                accessKeyId: AWS_ACCESS_KEY_ID,
+                region
+            });
+
+            const s3 = new AWS.S3({
+                apiVersion: "2006-03-01",
+                params: { Bucket: albumBucketName }
+            });
+
+            //Upload to S3 Bucket
+            s3.upload(
+                {
+                    Key: `${uuid.v4()}-${filename}`,
+                    Body: stream,
+                    ACL: "public-read"
+                },
+                (err, data) => {
+                    if (err) {
+                        console.log(
+                            `There was an error uploading your photo: ${
+                                err.message
+                            }`
+                        );
+                    }
+                    console.log(data);
+
+                    //View list of objects in S3 Bucket
+                    s3.listObjects({ Delimiter: "/" }, (err, data) => {
+                        if (err) {
+                            console.log(err.message);
+                        } else {
+                            console.log(data);
+                        }
+                    });
+                }
+            );
 
             return { stream, filename, mimetype, encoding };
         }
